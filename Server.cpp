484d6ba854b2a5ecab9673547ef7325644ba9f05
Maddie Van Buren <mvanbure@mail.umw.edu>
2024-12-07 22:13:16 -0500

    adding more features for the server

--- Server.cpp
+++ Server.cpp
@@ -31,21 +31,73 @@ Server::~Server(){
 }
 
 
-void Server::start() {
-	std::cout << "Please type out any message and type exit to stop our chat!";
+void Server::start(){
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
+        throw runtime_error("Error binding server socket.");
+    }
 
-	while (true) {
-		string message;
-		cout << ": ";
-		getline(cin, message);
+    if (listen(serverSocket, 5) == -1)
+    {
+        throw runtime_error("Error listening on server socket.");
+    }
 
-		if (message == "exit") {
-			std::cout << "Goodbye!" << endl;
-			break;
-		}
+    running = true;
+    cout << "Server started. Waiting for clients..." << endl;
 
-		sendMessage(message, -1);
-	}
+    while (running)
+    {
+        handleNewClient();
+    }
+
+}
+
+
+void Server :: newClient(){
+    sockaddr_in clientAddress;
+    socklen_t clientAddrLen = sizeof(clientAddress);
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddrLen);
+
+    if (clientSocket != -1)
+    {
+        lock_guard<mutex> lock(clientMutex);
+        clientSockets.push_back(clientSocket);
+        cout << "Client connected!" << endl;
+
+        thread([this, clientSocket]() {
+            char buffer[1024];
+            string username;
+            bool firstMessage = true;
+
+            while (true)
+            {
+                memset(buffer, 0, sizeof(buffer));
+                int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+                if (bytesRead <= 0)
+                {
+                    lock_guard<mutex> lock(clientMutex);
+                    clientSockets.erase(remove(clientSockets.begin(), clientSockets.end(), clientSocket), clientSockets.end());
+                    cout << username << " disconnected." << endl;
+                    close(clientSocket);
+                    break;
+                }
+
+                if (firstMessage)
+                {
+                    username = buffer;
+                    cout << username << " joined the chat." << endl;
+                    sendMessageToAll(username + " joined the server.", clientSocket);
+                    firstMessage = false;
+                }
+                else
+                {
+                    string message = username + ": " + buffer;
+                    sendMessageToAll(message, clientSocket);
+                }
+            }
+        }).detach();
+    }
+
 }
 
 void Server::sendMessage(const string& message, int senderSocket) {
